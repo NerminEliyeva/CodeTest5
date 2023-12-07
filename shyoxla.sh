11f9874afc20b3b2a9a6e383ad5afd277dfe70e9
NerminEliyeva <narminahmedova1997@gmail.com>
2023-12-07 23:31:05 +0400

    changes

--- shyoxla.sh
+++ shyoxla.sh
@@ -1,18 +1,43 @@
 
- printf "enter folder name : "
- read dirname
- mkdir $dirname
- cd $dirname
+#  printf "enter folder name : "
+#  read dirname
+#  mkdir $dirname
+#  cd $dirname
 
-echo 'Hello, world.' > $dirname
+# git add .
 
-git add .
+# # printf "enter commit message : "
+# # git commit -m "$comment"
+# git commit -m "sh test commit"
+# # read commit
+# git push 
 
-# printf "enter commit message : "
-# git commit -m "$comment"
-git commit -m "sh test commit"
-# read commit
-git push 
+# pause
+
+# Folder name daxil et
+printf "File name daxil edin: "
+read filename
+
+# ad soyad daxil et
+printf "Adinizi daxil edin: "
+read firstname
+
+printf "Soyadinizi daxil edin: "
+read lastname
+
+# ad soyadi foldere yazdir
+echo "İsim: $firstname" > "$filename"
+echo "Soyisim: $lastname" >> "$filename"
+
+# tesdiq mesaji goster
+printf "'$filename' fayli hazirdir."
+
+# commit mesaji daxil et
+printf "Commit mesaji daxil edin: "
+read commitmessage
+
+git add "$filename"
+git commit -m "$commitmessage"
+git push origin main
 
 pause
-
